86a4164199a98475cb8e8f291988308d1daee94b
Johan Janssens <johan@janssens.me>
2009-03-04 15:56:50 +0000

    Updated information

--- code/plugins/system/koowa/README.php
+++ code/plugins/system/koowa/README.php
@@ -11,13 +11,13 @@ defined('_JEXEC') or die('Restricted access');
 ?>
 
 Requirements
-————————————
+
 Due to a serious security vulnerability in all Joomla 1.5.x versions, it is 
 highly recommended to use 1.5.6 for all Joomla sites. Furthermore, PHP 5.2 and 
 MySQL 5.0 are required. Please check these before installing.
 
 Installation
-————————————
+
 Install the Koowa plugin through Joomla's extension installer. Make sure you 
 enable the plugin in the Plugin manager. Upgrading between 0.X versions 
 (eg 0.5 to 0.6) is not supported. Upgrading between 0.X.y versions (eg 0.5.0 
