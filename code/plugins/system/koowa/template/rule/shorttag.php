d6a9f8d9f6473b54975be8b9f7bfc54d8ca42266
Mathias Verraes <mathias@verraes.net>
2009-05-05 20:19:45 +0000

    Smarter short tag replacement, including white space variations

--- code/plugins/system/koowa/template/rule/shorttag.php
+++ code/plugins/system/koowa/template/rule/shorttag.php
@@ -29,28 +29,15 @@ class KTemplateRuleShorttag extends KObject implements KTemplateRuleInterface
         if (ini_get('short_open_tag')) {
         	return;
         }
-        
-        /**
-         * We could also convert <%= like the real T_OPEN_TAG_WITH_ECHO
-         * but that's not necessary.
-         * 
-         * It might be nice to also convert PHP code blocks <? ?> but 
-         * let's quit while we're ahead.  It's probably better to keep 
-         * the <?php for larger code blocks but that's your choice.  If
-         * you do go for it, explicitly check for <?xml as this will
-         * probably be the biggest headache.
-         */
-                
+   
         // convert "<?=" to "<?php echo"
-        $find = '/\<\?\=\s?(.*?)/';
+        $find = '/\<\?\s*=\s*(.*?)/';
         $replace = "<?php echo \$1";
         $text = preg_replace($find, $replace, $text);
-        
+
         // convert "<?" to "<?php"
-        $find = '/\<\?\s(.*?)/';
+        $find = '/\<\?(?:php)?\s*(.*?)/';
         $replace = "<?php \$1";
         $text = preg_replace($find, $replace, $text);
-        
-
 	}
 }
