5f03718e01f26661901b5bc70211d2ddff2d9adb
Johan Janssens <johan@janssens.me>
2009-12-04 10:32:23 +0000

    Implemented catch all handling for Exceptions.

--- code/plugins/system/koowa.php
+++ code/plugins/system/koowa.php
@@ -20,6 +20,8 @@ class plgSystemKoowa extends JPlugin
 {
 	public function __construct($subject, $config = array())
 	{
+		set_exception_handler(array($this, 'exceptionHandler'));
+		
 		require_once JPATH_PLUGINS.DS.'system'.DS.'koowa'.DS.'koowa.php';
 		require_once JPATH_PLUGINS.DS.'system'.DS.'koowa'.DS.'loader.php';
 			
@@ -43,5 +45,37 @@ class plgSystemKoowa extends JPlugin
 		JPluginHelper::importPlugin('koowa', null, true, KFactory::get('lib.koowa.event.dispatcher'));
 		
 		parent::__construct($subject, $config = array());
+	}
+	
+	/**
+	 * Catch all exception handler
+	 * 
+	 * Calls the Joomla error handler to process the exception
+	 * 
+	 * @param object an Exception object
+	 * @return void
+	 */
+	public function exceptionHandler($exception)
+	{
+		$this->_exception = $exception; //store the exception for later use
+		
+		//Change the Joomla error handler to our own local handler and call it
+		JError::setErrorHandling( E_ERROR,	'callback', array($this,'errorHandler'));
+		JError::raiseError('500', $exception->getMessage());
+	}
+	
+	/**
+	 * Custom JError callback
+	 * 
+	 * Push the exception call stack in the JException returned through the call back 
+	 * adn then rener the custom error page
+	 * 	  
+	 * @param object	A JException object
+	 * @return void
+	 */
+	public function errorHandler($error)
+	{
+		$error->backtrace = $this->_exception->getTrace();
+		JError::customErrorPage($error);
 	}
 }
